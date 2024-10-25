3a9c8a0d58738d4d37827e85e8503ea38171dca7
Ironviper <email@email.com>
2024-10-25 17:44:47 -0300

    set proxy

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    // proxy: { '/api': {target: 'http://localhost:5000/',
-    //   changeOrigin: true,
-    //   rewrite: (path) => path.replace(/^\/api/, '')
-    // } }
+    proxy: { '/api': {target: 'https://json-server-nine-brown.vercel.app/',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    } }
   },
 })

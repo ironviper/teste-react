9d36408eca7878efcdb5328aab45d7cbbfe29285
Ironviper <jvgarciam98@gmail.com>
2024-10-25 16:57:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-       proxy: { '/api': {target: 'https://teste-react-ten-nu.vercel.app/',
-       changeOrigin: true,
-       rewrite: (path) => path.replace(/^\/api/, '')
-    } }
+       }
   },
 })
